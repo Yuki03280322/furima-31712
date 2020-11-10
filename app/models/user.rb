8d3adb0c4fa8b366b02428780a1caf2aef8adad2
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-10 21:47:05 +0900

    バリデーション、画面遷移実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :email, uniqueness: { case_sensitive: true }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
+  validates :password_confirmation, :password, confirmation: true
+  validates :date_of_birth, presence: true  
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } do
+    validates :first_name_kanji
+    validates :last_name_kanji
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
 end
+
