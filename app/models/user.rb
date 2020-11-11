ab741804adde4f9bd385f6dc07d2ae6f7ea7552e
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-11 17:01:29 +0900

    【修正】presence: trueをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,20 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password_confirmation
+    validates :first_name_kanji
+    validates :last_name_kanji
+    validates :first_name_kana
+    validates :last_name_kana
     validates :date_of_birth
   end
 
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字(漢字,ひらがな,カタカナ)を使用してください' } do
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字(漢字,ひらがな,カタカナ)を使用してください' } do
     validates :first_name_kanji
     validates :last_name_kanji
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+  with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
