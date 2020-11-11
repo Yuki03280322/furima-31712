b94f01aad410f92a820f93f6212220849d1c94a1
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-11 16:58:14 +0900

    【修正】presence: trueをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
   validates :email, uniqueness: { case_sensitive: true }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
-  validates :password_confirmation, presence: true
   validates :password_confirmation, :password, confirmation: true
-  validates :date_of_birth, presence: true  
+
+  with_options presence: true do
+    validates :nickname
+    validates :password_confirmation
+    validates :date_of_birth
+  end
+
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字(漢字,ひらがな,カタカナ)を使用してください' } do
     validates :first_name_kanji
