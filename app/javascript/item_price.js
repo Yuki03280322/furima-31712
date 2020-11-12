dd99ca3891239659ca36679725632d85a7a49d3e
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2020-11-12 21:17:40 +0900

    【修正take2】JavaScriptファイル統一とActiveHashのモデル等数点削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,6 +4,8 @@ function itemPrice() {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue / 10);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
   })
 }
 
